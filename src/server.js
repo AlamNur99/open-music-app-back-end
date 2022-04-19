fe6a86c96eb63b93f76473f5b44ce25f4f345220
Muhamma Alam Nur Haris <muh.alamnur.haris99@gmail.com>
2022-04-19 16:26:23 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/album');
+const songs = require('./api/song');
+const AlbumService = require('./services/postgres/AlbumService');
+const SongService = require('./services/postgres/SongService');
+const AlbumValidator = require('./validator/album');
+const SongValidator = require('./validator/song');
+
+const init = async () => {
+    const albumService = new AlbumService();
+    const songService = new SongService();
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: process.env.HOST,
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    await server.register([{
+        plugin: albums,
+        options: {
+            service: albumService,
+            validator: AlbumValidator,
+        },
+    }, {
+        plugin: songs,
+        options: {
+            service: songService,
+            validator: SongValidator
+        }
+    }]);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
